1f5ac1217e0a9a7c455b17403a4b866edb30e003
Barry Hanna <barryahanna@gmail.com>
2021-06-15 13:35:47 +0100

    Fixed order summary and order summary modals

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+// Default plan options, used to populate initial order
+// summary and order summary modal.
+const planOptions = {
+  packaged: 'capsule',
+  type: 'single',
+  amount: '250g',
+  grind: 'wholebean',
+  frequency: 'Every week',
+};
+
 const toggleBtns = document.querySelectorAll('.toggle-show-btn');
 
+// Order Summary radio buttons
 const orderSummaryPackaged = document.getElementById('order-summary-packaged');
 const orderSummaryType = document.getElementById('order-summary-type');
 const orderSummaryAmount = document.getElementById('order-summary-amount');
@@ -8,6 +19,7 @@ const orderSummaryFrequency = document.getElementById(
   'order-summary-frequency'
 );
 
+// Order summary modal - to be updated.
 const orderSummaryModalPackaged = document.getElementById(
   'order-summary-modal-packaged'
 );
@@ -23,13 +35,50 @@ const orderSummaryModalGrind = document.getElementById(
 const orderSummaryModalFrequency = document.getElementById(
   'order-summary-modal-frequency'
 );
+
 const form = document.getElementById('coffee-form');
 
-console.log(form.elements['packaged'].value);
-console.log(form.elements['type'].value);
-console.log(form.elements['amount'].value);
-console.log(form.elements['grind'].value);
-console.log(form.elements['frequency'].value);
+const planBtn = document.querySelector('.plan-btn');
+
+planBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  document.querySelector('.order-summary-modal').classList.toggle('open');
+});
+
+Array.from(form.elements).forEach((element) =>
+  element.addEventListener('change', updateOrder)
+);
+
+function updateOrder() {
+  planOptions.packaged = form.elements['packaged'].value;
+  planOptions.type = form.elements['type'].value;
+  planOptions.amount = form.elements['amount'].value;
+  planOptions.grind = form.elements['grind'].value;
+  planOptions.frequency = form.elements['frequency'].value;
+  updateOrderSummaries();
+}
+
+// Update the Order Summary and Order Summary Modal
+function updateOrderSummaries() {
+  updateOrderSummary();
+  updateOrderSummaryModal();
+}
+
+function updateOrderSummary() {
+  orderSummaryPackaged.innerText = planOptions['packaged'];
+  orderSummaryType.innerText = planOptions['type'];
+  orderSummaryAmount.innerText = planOptions['amount'];
+  orderSummaryGrind.innerText = planOptions['grind'];
+  orderSummaryFrequency.innerText = planOptions['frequency'];
+}
+
+function updateOrderSummaryModal() {
+  orderSummaryModalPackaged.innerText = planOptions['packaged'];
+  orderSummaryModalType.innerText = planOptions['type'];
+  orderSummaryModalAmount.innerText = planOptions['amount'];
+  orderSummaryModalGrind.innerText = planOptions['grind'];
+  orderSummaryModalFrequency.innerText = planOptions['frequency'];
+}
 
 toggleBtns.forEach((button) =>
   button.addEventListener('click', (e) => {
@@ -42,8 +91,11 @@ function toggleOption(element) {
   element.closest('.coffee-option').classList.toggle('open');
 }
 
-function initialiseOrderSummary() {
-  console.log('Initialised');
-}
+// Dismiss modal when checkout button is clicked.
+const checkoutBtn = document.querySelector('.checkout-btn');
 
-document.onload = initialiseOrderSummary();
+checkoutBtn.addEventListener('click', (e) => {
+  e.currentTarget.closest('.order-summary-modal').classList.toggle('open');
+});
+
+document.onload = updateOrderSummaries();
